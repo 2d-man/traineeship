60cd0a6e2a6d60653bfe0db5a5dbeb828f3fbd81
2d-man <v.belyakov1997@gmail.com>
2023-09-22 16:03:43 +0300

    add VCheckbox

---
+++ teachersschedule/src/components/TeachersSchedule/components/VCheckbox.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts" generic="T extends object">
+import { ref } from 'vue'
+
+export interface Props {
+  visible: boolean
+  // disable: boolean
+}
+
+const prop = withDefaults(defineProps<Props>(), {
+  visible: true,
+  // disable: false,
+})
+
+const emit = defineEmits<{
+  change: []
+}>()
+
+const switchLabel = ref('Показать поле ввода')
+
+function toggleTextVisible() {
+  if (switchLabel.value === 'Показать поле ввода')
+    switchLabel.value = 'Скрыть поле ввода'
+  else switchLabel.value = 'Показать поле ввода'
+}
+</script>
+
+<template>
+  <div>
+    <input id="checkbox" type="checkbox" @change="toggleTextVisible">
+    <label for="checkbox"> {{ switchLabel }} </label>
+  </div>
+</template>
