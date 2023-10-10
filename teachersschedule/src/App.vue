0de3727ffe083f5734828581ed647b540b3be8c3
melnikovbogdan <maestro.25@mail.ru>
2023-10-10 16:41:39 +0300

    chore: add suspense

--- teachersschedule/src/App.vue
+++ teachersschedule/src/App.vue
@@ -3,11 +3,18 @@ import TeachersSchedule from '../src/components/TeachersSchedule/TeachersSchedul
 </script>
 
 <template>
-  <div class="w-screen min-h-screen flex p-10 justify-center items-center bg-gray-100">
-    <div class="w-full min-h-full flex flex-col gap-y-4 p-10  bg-white rounded-2xl shadow">
-      <div class="flex flex-col">
-        <TeachersSchedule />
+  <Suspense>
+    <template #default>
+      <div class="w-screen min-h-screen flex p-10 justify-center items-center bg-gray-100">
+        <div class="w-full min-h-full flex flex-col gap-y-4 p-10  bg-white rounded-2xl shadow">
+          <div class="flex flex-col">
+            <TeachersSchedule />
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </template>
+    <template #fallback>
+      Гружусь...
+    </template>
+  </Suspense>
 </template>
