c949f62ffcc85e9bc76df1f83b53531734a8ad59
2d-man <v.belyakov1997@gmail.com>
2023-10-16 17:37:55 +0300

    feet: add VCard.vue

---
+++ teachersschedule/src/components/TeachersSchedule/components/VCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+export interface Props {
+  name: string
+  email: string
+  isCringe: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="block min-w-20 mb-2 px-4 py-1 m-1 rounded bg-orange-600 text-white duration-150 focus:bg-orange-700 hover:bg-red-600"
+  >
+    <div>
+      Имя: {{ name }}
+    </div>
+    <div>
+      Почта: {{ email }}
+    </div>
+    <div>
+      Кринге: {{ isCringe }}
+    </div>
+  </div>
+</template>
